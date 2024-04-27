992ea8a96fa1e9d1a6b9e9ecbdb5f253c180cb83
Thalisson Castro <adaothalisson@hotmail.com>
2024-04-26 22:25:44 -0300

    modified the phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Já viu aquele filme “O Curioso Caso de Benjami-nha boca?</h1>
+    return <h1>Já viu aquele filme “O Curioso Caso de Benjami-nha boca? ;)</h1>
 }
 
 export default Home;
