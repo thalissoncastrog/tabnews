da9756bc6c83b3a6c97cf6d9e0ceacc4c92bf6ee
Thalisson Castro <adaothalisson@hotmail.com>
2024-04-26 22:36:07 -0300

    modified phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>PSIUUUU... NÃO ESQUECE!!!\nTU É MARAVILHOSA "VISSE" ❤️</h1>
+    return <h1>PSIUUUU... NÃO ESQUECE!!!
+            TU É MARAVILHOSA "VISSE" ❤️</h1>
 }
 
 export default Home;
