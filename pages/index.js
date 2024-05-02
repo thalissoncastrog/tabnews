325a4fb1aac931b938143e79dd6ea7870e0f4047
Thalisson Castro <adaothalisson@hotmail.com>
2024-05-02 10:37:02 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>PSIUUUU... NÃO ESQUECE!!!TU É lindonaaa "VISSE" ❤️</h1>
+  return <h1>PSIUUUU... NÃO ESQUECE!!!TU É lindonaaa "VISSE" ❤️</h1>;
 }
 
+function test() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("teste de indentação");
+}
 export default Home;
